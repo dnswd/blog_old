9ce1023a6ee2ba6b479ff61949a32d9fb83777ec
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2023-04-15 01:54:34 +0700

    responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <nav className="fixed pt-home-padding w-full z-10">
         <div className="flex justify-between m-auto w-screen-65">
           <Link href="/">dnswd.id</Link>
-          <ul className="flex flex-row gap-16">
+          <ul className="hidden sm:flex flex-row sm:gap-16">
             <li>
               <Link href="/blog">blog</Link>
             </li>
@@ -25,9 +25,9 @@ export default function Home() {
       </nav>
       <main className="h-screen">
         <div className="absolute left-0 top-0 w-full">
-          <header className="relative my-80 mx-auto w-screen-65">
+          <header className="relative my-40 sm:my-80 mx-auto w-screen-65">
             <div className="w-screen-50">
-              <p className="text-7xl">
+              <p className="text-5xl sm:text-7xl">
                 I’m Thieb — a Multidisciplinary Designer with a focus on Motion, Interactive Design and 3D.
               </p>
             </div>
@@ -36,15 +36,15 @@ export default function Home() {
               <div className="absolute h-5 w-5 bottom-6 left-1/2 border-t-2 border-r-2 border-black rotate-135 origin-top-left-5-95"></div>
             </div>
           </header>
-          <section className="flex my-40-rem mx-auto w-screen-65 justify-between">
-            <article className="flex flex-col gap-28 w-3/5 text-5xl">
+          <section className="flex flex-col sm:flex-row my-96 sm:my-40-rem mx-auto w-screen-65 justify-between gap-16 sm:gap-0">
+            <article className="flex flex-col gap-28 w-full sm:w-3/5 text-3xl sm:text-5xl">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati ex distinctio enim sapiente maiores, facere alias, ratione assumenda, nobis reiciendis. Unde, ratione laborum nostrum officiis modi sunt id eum?</p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati ex distinctio enim sapiente maiores, facere alias, ratione assumenda, nobis reiciendis. Unde, ratione laborum nostrum officiis modi sunt id eum?</p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati ex distinctio enim sapiente maiores, facere alias, ratione assumenda, nobis reiciendis. Unde, ratione laborum nostrum officiis modi sunt id eum?</p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati ex distinctio enim sapiente maiores, facere alias, ratione assumenda, nobis reiciendis. Unde, ratione laborum nostrum officiis modi sunt id eum?</p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto obcaecati ex distinctio enim sapiente maiores, facere alias, ratione assumenda, nobis reiciendis. Unde, ratione laborum nostrum officiis modi sunt id eum?</p>
             </article>
-            <div className="flex flex-col gap-16 w-1/5 text-xl">
+            <div className="flex flex-col gap-16 w-full sm:w-1/5 text-xl">
               <article className="flex flex-col gap-8">
                 <h2>experience</h2>
                 <p className="leading-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta vitae laborum recusandae fugit laudantium doloribus adipisci ab unde ad voluptatibus doloremque quis expedita aut delectus dolorum aspernatur, ex obcaecati laboriosam.</p>
