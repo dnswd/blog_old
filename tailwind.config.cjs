f350c47d9ed350ce4f38340faeacfe3768f125a8
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2023-04-15 00:25:12 +0700

    feat: fluid type

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
+  // mode: "jit",
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -17,10 +17,22 @@ module.exports = {
       spacing: {
         'home-padding': '3.6rem',
         '40-rem': '40rem',
-      }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'font-size':
+              'clamp(1.125rem, calc(1.125rem + ((1.25 - 1.125) * ((100vw - 20rem) / (96 - 20)))), 1.25rem)',
+          },
+        },
+      },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  corePlugins: {
+    fontSize: false,
+},
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-fluid-type"),
+  ],
 };
-
-// calc(20px + (72px - 20px) * ((100vw - 640px) / (1536px - 640px)));
